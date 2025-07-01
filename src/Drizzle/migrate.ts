0cd4c586809ac0744a4506fb98ef9f11db00a3ef
Joemark <joegichangi@gmail.com>
2025-07-01 12:50:03 +0300

    commented migrate.ts

--- src/Drizzle/migrate.ts
+++ src/Drizzle/migrate.ts
@@ -1,19 +1,19 @@
-import "dotenv/config";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
-import db, { client } from "./db";
+// import "dotenv/config";
+// import { migrate } from "drizzle-orm/node-postgres/migrator";
+// import db, { client } from "./db";
 
-async function migration() {
-    try {
-        console.log("......Migrations Started......");
-        await migrate(db, { migrationsFolder: __dirname + "/migrations" });
-        console.log("......Migrations Completed......");
-    } catch (error) {
-        console.error("Migration failed:", error);
-        process.exit(1);
-    } finally {
-        await client.end();
-        process.exit(0);
-    }
-}
+// async function migration() {
+//     try {
+//         console.log("......Migrations Started......");
+//         await migrate(db, { migrationsFolder: __dirname + "/migrations" });
+//         console.log("......Migrations Completed......");
+//     } catch (error) {
+//         console.error("Migration failed:", error);
+//         process.exit(1);
+//     } finally {
+//         await client.end();
+//         process.exit(0);
+//     }
+// }
 
-migration();
+// migration();
